35ad26eee526ab477112dd31094777086a2d8166
Fikremariam Mekonnen <fikremariam2012@gmail.com>
2025-03-10 16:08:16 +0300

    Updates

--- src/Components/API/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const axiosInstance = axios.create({
-    // baseURL: "http://localhost:5000/"
-    baseURL: " https://amazon-api-deployment-ojd9.onrender.com/"
-
-})
-
-export {axiosInstance}
