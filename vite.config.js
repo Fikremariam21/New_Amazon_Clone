a33938ca1cb17581bccf997014ab70b31143fe6f
Fikremariam Mekonnen <fikremariam2012@gmail.com>
2025-03-10 11:46:21 +0300

    Finall deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
-  base: isProduction ? "/New_Amazon_Clone/" : "/",
-  // base: "/New_Amazon_Clone/",
+  base: "/New_Amazon_Clone/",
 })
